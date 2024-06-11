ee1d06a0409b857789149c56907c6459f0f1b04c
shaping520 <144744478+shaping520@users.noreply.github.com>
2024-06-11 17:14:25 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -29,7 +29,7 @@ crawl_type = "3"
 # 收集其他大佬url中的直播源
 crawl_urls = [
     "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt",
-    "https://d.kstore.space/download/6851/zby.txt"
+    # "https://d.kstore.space/download/6851/zby.txt"
 ]
 # ipv6源检测有效性的代理地址，用于不支持ipv6网络的主机，若网络支持ipv6，这里填空""
 ipv6_proxy = "http://www.ipv6proxy.net/go.php?u=" #此代理用于github，国内不一定能用
