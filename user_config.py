876e332141b47d4197652be8932261c539947b67
shaping520 <144744478+shaping520@users.noreply.github.com>
2024-06-30 22:18:42 +0800

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -28,7 +28,7 @@ search_ignore_key = ["高清", "4K"]
 crawl_type = "3"
 # 收集其他大佬url中的直播源
 crawl_urls = [
-    # "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt",
+    "https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt",
     # "https://d.kstore.space/download/6851/zby.txt"
 ]
 # ipv6源检测有效性的代理地址，用于不支持ipv6网络的主机，若网络支持ipv6，这里填空""
